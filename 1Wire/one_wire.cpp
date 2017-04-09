fa00f5fbac8297491fe6917c2c41d3d3108ca15e
lit-uriy <lit-uriy@yandex.ru>
2017-04-09 13:38:50 +0500

    1) Накопипастил из своего AVR-ного кода шпргалок; 2) Сделал Reset/Presence, но есть вопросы.

--- 1Wire/one_wire.cpp
+++ 1Wire/one_wire.cpp
@@ -1,6 +1,66 @@
 #include "one_wire.h"
 
-OneWire::OneWire()
+OneWire::OneWire(DigitalInOut apin)
+    : _pin(apin)
 {
 
 }
+
+LineStatus OneWire::reset()
+{
+    // в начале на шине должна быть "1"
+    if (!pin())
+        return StatusShortCircuit;
+    // выставляем "Reset pulse"
+    pinLow();
+    // выдержваем в течении TRSTL
+    deleyUs(TimeReset);
+
+    // снимаем "Reset pulse"
+    pinRelease();
+    // ждем возврата линии "0"->"1" (слэйв подождёт TimePdh, а затем выставит Presence на время TimePdl)
+    deleyUs(TimeRelease);
+    // на шине должна стать "1"
+    if (!pin())
+        return StatusShortCircuit;
+
+    // подождем время, за которое слэйв гарантировано выставит "Presence pulse"
+    deleyUs(TimeSlot);
+    // проверим наличие "Presence pulse"
+    if (pin())
+        return StatusAbsent;
+
+    //ждем завершения "Presence pulse" "0"->"1"
+    deleyUs(TimePresence);
+    // на шине должна стать "1"
+    if (!pin())
+        return StatusShortCircuit;
+
+    // нормальный "Presence pulse" был
+    return StatusPresence;
+}
+
+// 0x33 или 0x0F - старая таблетка DS1990 (без буквы А)
+void OneWire::readROM()
+{
+
+}
+
+// 0x55
+void OneWire::matchROM()
+{
+
+}
+
+// 0xF0
+void OneWire::searchROM()
+{
+
+}
+
+// 0xCC
+void OneWire::skipROM()
+{
+
+}
+
--- 1Wire/one_wire.h
+++ 1Wire/one_wire.h
@@ -1,11 +1,112 @@
 #ifndef ONEWIRE_H
 #define ONEWIRE_H
 
+#include "mbed.h"
+
+//-------------------------
+
+
+//    0x60=480us, 0x01=5us  0x01=8us
+#define T15 0x02				//15us
+#define Tpdh 0x07				//15...60us  0x07 (номинал 30us)
+#define Tslot 0x0F			//60...120us 0x0F
+#define Tpdl 0x1E				//60...240us 0x1E (120us)
+#define Trstl 0x3C			//>=480us    0x3C
+
+//-------------------------
+
+#define T_SLOT	18			//длительность слота
+#define T_10U	(T_SLOT/6)	//10мкс
+#define T_RSTL	(8*T_SLOT)	//длительность сброса (reset)
+#define T_RELS	(T_SLOT*2/5)//длительность востановления (release)
+#define T_PRES	(4*T_SLOT)	//длительность отклика (presence)
+
+
+
+#define delay_TM(del)	TMR2Delay(del)//задержка
+
+#define TM_PRESNC		0x00	//TM присутствует
+#define TM_SHRT_CRT		0x01	//Линия TM закорочена
+#define TM_ABSNT		0x02	//TM отсутствует
+
+#define TM_CMD_READROM		0x33	//Чтение ПЗУ (ID)
+#define TM_CMD_MATCHROM		0x55	//Обращение по адресу
+#define TM_CMD_SEARCHROM	0xF0	//поиск ПЗУ (ID)
+#define TM_CMD_SKIPROM		0xCC	//пропустить ПЗУ (ID)
+
+//-------------------------
+
+
+
+
 
 class OneWire
 {
 public:
-    OneWire();
+    OneWire(DigitalInOut apin);
+
+    // ROM function commands - Network layer
+protected:
+    enum LineStatus {
+        StatusPresence = 0,
+        StatusShortCircuit,
+        StatusAbsent,
+    };
+
+
+    /**
+     * @brief reset
+     * @return признак присутствия на шине
+     */
+    LineStatus reset();
+
+    enum RomCommands {
+        CommandReadRom = 0x33,
+        CommandMatchRom = 0x55,
+        CommandSearchRom = 0xF0,
+        CommandSkipRom = 0xCC,
+    };
+
+    void readROM(); // 0x33 (или 0x0F - старая таблетка DS1990, без буквы А)
+    void matchROM(); // 0x55
+    void searchROM(); // 0xF0
+    void skipROM(); // 0xCC
+
+    enum Times {
+        // из "Book of iButton Standards (AN937)"
+        TimeSlot = 60,
+        TimePdh = TimeSlot/2,
+        TimePdl = 2*TimeSlot,
+        TimeReset = 8*TimeSlot,
+        // расчётное/подобранное
+        TimeRelease = 2*TimeSlot/5,
+        TimePresence = 4*TimeSlot
+    };
+
+private:
+    DigitalInOut _pin;
+
+    inline bool pin()
+    {
+        return _pin->read();
+    }
+
+    inline void pinLow()
+    {
+        _pin->write(0);
+    }
+
+    inline void pinRelease()
+    {
+        _pin->write(1);
+    }
+
+    inline void deleyUs(int us)
+    {
+        wait_us(us);
+    }
+
+
 };
 
 #endif // ONEWIRE_H
