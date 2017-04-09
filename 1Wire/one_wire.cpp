9e0f9ce34dfbb724d73a7d347c38e2e30fa1c351
lit-uriy <lit-uriy@yandex.ru>
2017-04-09 23:27:47 +0500

    1Wire: отформатировал код вычисления CRC.

--- 1Wire/one_wire.cpp
+++ 1Wire/one_wire.cpp
@@ -116,14 +116,16 @@ OneWire::LineStatus OneWire::readWriteByte(unsigned char *byte)
 
 unsigned char OneWire::crc8(unsigned char data, unsigned char crc8val)
 {
-unsigned char cnt;
-cnt=8;
+    unsigned char cnt;
+    cnt=8;
     do{
         if ((data^crc8val)&0x01){
             crc8val^=0x18; //0x18-Полином:0b00011000=>X^8+X^5+X^4+1
             crc8val>>=1;
             crc8val|=0x80;}
-        else {crc8val>>=1;}
+        else {
+            crc8val>>=1;
+        }
         data>>=1;
     }while (--cnt);
     return crc8val;
