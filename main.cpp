8860ba35547feb65b643cbce91a5a28307146869
lit-uriy <lit-uriy@yandex.ru>
2017-04-15 12:29:44 +0500

    Сделал отладку DS1820 отключаемой.

--- main.cpp
+++ main.cpp
@@ -46,12 +46,11 @@ char rom[2*8+1]; // в два раза больше символов + замы�
     }else{
 
         printf("Finding devices...\r\n");
-
+#ifdef DS1820_TEST
         DS1820 dev(DATA_PIN);
         dev.romCode(rom);
         printf("ROM code = %s\r\n", rom);
-
-        while(1){wait(1);};
+#else
         // Initialize the probe array to DS1820 objects
         int num_devices = 0;
         while(DS1820::unassignedProbe(DATA_PIN)) {
@@ -77,6 +76,7 @@ char rom[2*8+1]; // в два раза больше символов + замы�
 
             while(1){wait(1);};
         }
+#endif
     }
 }
 
