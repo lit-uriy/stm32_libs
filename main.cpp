732edfe778a50d401a500532173f3d37dcce1516
Yuriy (Юрий) <lit-uriy@sibna.ru>
2020-10-15 11:46:11 +0500

    Нахомячиваю свой новый вариант тестилки термометров.

--- main.cpp
+++ main.cpp
@@ -1,96 +1,96 @@
-#define MULTIPLE_PROBES
-#define DATA_PIN        A0
-
-
-#ifdef MULTIPLE_PROBES
-
 #include "mbed.h"
-#include "DS1820.h"
 #include "Containers/ylist.h"
 
 #include "1Wire/one_wire.h"
 #include "1Wire/y_ds1820.h"
 
-#define MAX_PROBES      16
- 
-DS1820 *probe[MAX_PROBES];
 
-DigitalIn mybutton(USER_BUTTON);
+DigitalInOut oneWirePin(D7); // ножка для 1-Wire
+
+DigitalIn buttonStartFind(D2); // Начать поиск 1-Wire-устройств
+DigitalIn buttonNextFind(D3);  // Поиск следующего 1-Wire-устройства
+
+DigitalIn buttonPrintNextTemp(USER_BUTTON);  // Печать следующего термометра
+DigitalIn buttonPrintAllTemp(D4);  // Непрерывное измерение по всем термометрам
+
+DigitalOut ledShortingWire(D8); // сигнал на светодиод о КЗ на 1-Wire
 
 DigitalOut syncroPin(A1);
 bool test = false;
 
 
- 
-int main() {  
-
-    printf("\r\n----------------------------\r\n");
-
 char romString[2*8+1]; // в два раза больше символов + замыкающий нуль
 
 
+void startFind();
+void nextFind();
+void nextPrintNext();
+void nextPrintAll();
 
-    if (mybutton == 0) { // Button is pressed
-        printf("Button is pressed\r\n");
+ 
+int main() {  
+
+    printf("\r\n---------- RESET ------------------\r\n");
 
 
-
-//        test1();
-//        test2();
-//        test3();
-//        test3_1();
-        test4();
-//        test5();
-
-    }else{
-
-        printf("Finding devices...\r\n");
-#ifdef DS1820_TEST
-        DS1820 dev(DATA_PIN);
-        dev.romCode(rom);
-        printf("ROM code = %s\r\n", rom);
-#else
-        // Initialize the probe array to DS1820 objects
-        int num_devices = 0;
-        while(DS1820::unassignedProbe(DATA_PIN)) {
-            DS1820 *dev = new DS1820(DATA_PIN);
-            probe[num_devices] = dev;
-            dev->romCode(romString);
-            printf("Found %d device, ROM=%s\r\n\n", num_devices+1, romString);
-            num_devices++;
-            if (num_devices == MAX_PROBES)
-                break;
-        }
-        if (num_devices){
-            printf("Found %d device(s)\r\n\n", num_devices);
-            while(1) {
-                probe[0]->convertTemperature(true, DS1820::all_devices);         //Start temperature conversion, wait until ready
-                for (int i = 0; i<num_devices; i++)
-                    printf("Device %d returns %3.1f %sC\r\n", i, probe[i]->temperature(), (char*)(248));
-                printf("\r\n");
-                wait(1);
-            }
-        }else{
-            error("No devices!\r\n");
-
-            while(1){wait(1);};
-        }
-#endif
+    while (1) {
+        if (buttonStartFind)
+            startFind();
+        else if (buttonNextFind)
+            nextFind();
+        else if (buttonPrintNextTemp)
+            nextPrintNext();
+        else if (buttonPrintAllTemp)
+            nextPrintAll();
+        else
+            wait(0.1);
     }
 }
 
-#else
-#include "mbed.h"
-#include "DS1820.h"
- 
-DS1820 probe(DATA_PIN);
- 
-int main() {
-    while(1) {
-        probe.convertTemperature(true, DS1820::all_devices);         //Start temperature conversion, wait until ready
-        printf("It is %3.1foC\r\n", probe.temperature());
-        wait(1);
-    }
+//===========================================================================================
+//  Начинаем поиск 1-Wire устройств сначала
+//===========================================================================================
+void startFind()
+{
+
 }
 
-#endif
+
+
+
+
+
+//===========================================================================================
+//  Продолжаем поиск следующего 1-Wire устройства
+//===========================================================================================
+void nextFind()
+{
+
+}
+
+
+
+
+
+
+//===========================================================================================
+//  Печатаем температуру следующего термометра
+//===========================================================================================
+void nextPrintNext()
+{
+
+}
+
+
+
+
+
+
+//===========================================================================================
+//  Печатаем температуры всех термометров
+//===========================================================================================
+void nextPrintAll()
+{
+
+}
+
