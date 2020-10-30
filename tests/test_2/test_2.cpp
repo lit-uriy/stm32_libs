62b1768aa167dc7687a8b822baa88d5c1800f936
Yuriy (Юрий) <lit-uriy@yandex.ru>
2020-10-31 00:13:12 +0500

    Test-2: Заменил умалчиваемые printf/scanf-ы, на методы класса Serial.

--- tests/test_2/test_2.cpp
+++ tests/test_2/test_2.cpp
@@ -22,17 +22,19 @@ void convertTemperature(DS1820 *dev, int num_device);
 void printTemperature(float temp, int num_devices);
 void printRam(DS1820 *dev, int num_devices);
 
+Serial port(USBTX, USBRX);
+
 int main() {
 
 
     char command[80];
 
     while(1){
-        printf("\r\n------------ Ready ----------------\r\n");
-        scanf("%s", command);
+        port.printf("\r\n------------ Ready ----------------\r\n");
+        port.scanf("%s", command);
 
         if (strncmp(command , "test1()", sizeof("test1()")) == 0){
-            printf("Command test1(): Finding multiple devices...\r\n");
+            port.printf("Command test1(): Finding multiple devices...\r\n");
             DS1820 *probe[MAX_PROBES];
 
             // Initialize the probe array to DS1820 objects
@@ -45,7 +47,7 @@ int main() {
                     break;
             }
             if (num_devices){
-                printf("Found %d device(s)\r\n\n", num_devices);
+                port.printf("Found %d device(s)\r\n\n", num_devices);
 
                 while(1) {
                     convertTemperature(probe[0], 0);
@@ -55,30 +57,30 @@ int main() {
                         temp += t;
                         printTemperature(t, i+1);
                     }
-                    printf("Mean temperature: %3.1f %sC\r\n", temp/num_devices, (char*)(248));
-                    printf("\r\n");
+                    port.printf("Mean temperature: %3.1f %sC\r\n", temp/num_devices, (char*)(248));
+                    port.printf("\r\n");
                 }
             }else{
 //                error("No devices!\r\n");
-                printf("No devices!\r\n");
+                port.printf("No devices!\r\n");
             }
 
         }else if (strncmp(command , "test2()", sizeof("test2()")) == 0){
-            printf("Command test2(): Finding single devices...\r\n");
+            port.printf("Command test2(): Finding single devices...\r\n");
 
 
             DS1820 *probe = makeDevice(DATA_PIN, 1);
 
 
-            printf("Found %d device(s)\r\n\n", 1);
+            port.printf("Found %d device(s)\r\n\n", 1);
 
             while(1) {
                 convertTemperature(probe, 1);
                 printTemperature(probe->temperature(), 1);
-                printf("\r\n");
+                port.printf("\r\n");
             }
         }else{
-            printf("Unknown command: %s\r\n", command);
+            port.printf("Unknown command: %s\r\n", command);
         }
     }
 }
@@ -90,13 +92,13 @@ DS1820* makeDevice(PinName name, int num_devices)
 
     DS1820 *dev = new DS1820(name);
     dev->romCode(romString);
-    printf("Found %d device, ROM=%s\r\n", num_devices, romString);
-    printf("\tparasite powered: %s\r\n", dev->isParasitePowered()? "Yes": "No");
+    port.printf("Found %d device, ROM=%s\r\n", num_devices, romString);
+    port.printf("\tparasite powered: %s\r\n", dev->isParasitePowered()? "Yes": "No");
     char ramString[2*9+1]; // в два раза больше символов + замыкающий нуль
     dev->ramToHex(ramString);
-    printf("\tRAM: %s\r\n", ramString);
-    printf("\tresolution: %d bits\r\n", dev->resolution());
-    printf("\r\n");
+    port.printf("\tRAM: %s\r\n", ramString);
+    port.printf("\tresolution: %d bits\r\n", dev->resolution());
+    port.printf("\r\n");
 
     return dev;
 }
@@ -108,7 +110,7 @@ void convertTemperature(DS1820 *dev, int num_device)
 
 void printTemperature(float temp, int num_devices)
 {
-    printf("Device %d returns %3.1f %sC\r\n", num_devices, temp, (char*)(248));
+    port.printf("Device %d returns %3.1f %sC\r\n", num_devices, temp, (char*)(248));
 }
 
 
@@ -116,5 +118,5 @@ void printRam(DS1820 *dev, int num_devices)
 {
     char ramString[2*9+1]; // в два раза больше символов + замыкающий нуль
     dev->ramToHex(ramString);
-    printf("Device %d RAM: %s\r\n", num_devices, ramString);
+    port.printf("Device %d RAM: %s\r\n", num_devices, ramString);
 }
